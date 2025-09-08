5cc07e6bed3a778fc5a8ed01512f275001915163
Brandon Guigo <brandon.guigo@gmail.com>
2025-09-08 10:02:05 +0200

    fix: make the cupertino icon used inside the navbar
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/components/app/ab_navbar.dart
+++ lib/components/app/ab_navbar.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:ab_shared/utils/constants.dart';
 import 'package:ab_shared/utils/shortcuts.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// A customizable bottom navigation item for use with [BottomNavigation].
@@ -67,15 +70,12 @@ class NavigationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return Icon(icon);
+    return Icon(icon);
   }
 }
 
 class NavigationSection {
-  const NavigationSection({
-    required this.key,
-    required this.items,
-  });
+  const NavigationSection({required this.key, required this.items});
 
   /// The key of the section.
   final Key key;
@@ -83,8 +83,6 @@ class NavigationSection {
   /// The list of items in the section.
   final List<NavigationItem> items;
 }
-
-
 
 class ABNavbar extends StatefulWidget {
   final Function(String key) onPrimaryMenuSelected;
@@ -94,7 +92,15 @@ class ABNavbar extends StatefulWidget {
   final Color? backgroundColor;
   final Function(int)? onTap;
   final double height = 65;
-  const ABNavbar({super.key, required this.onPrimaryMenuSelected, required this.onSecondaryMenuSelected, required this.destinations, required this.primaryMenuKey, this.backgroundColor, this.onTap});
+  const ABNavbar({
+    super.key,
+    required this.onPrimaryMenuSelected,
+    required this.onSecondaryMenuSelected,
+    required this.destinations,
+    required this.primaryMenuKey,
+    this.backgroundColor,
+    this.onTap,
+  });
 
   @override
   State<ABNavbar> createState() => _ABNavbarState();
@@ -108,103 +114,143 @@ class _ABNavbarState extends State<ABNavbar> {
       height: widget.height,
       decoration: BoxDecoration(
         color: widget.backgroundColor ?? getTheme(context).primaryContainer,
-        borderRadius:
-                BorderRadius.circular($constants.corners.full),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.06),
-                offset: const Offset(0, 2),
-                blurRadius: 8,
-                spreadRadius: 0,
-              ),
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.04),
-                offset: const Offset(0, 8),
-                blurRadius: 16,
-                spreadRadius: -4,
-              ),
-            ]
+        borderRadius: BorderRadius.circular($constants.corners.full),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.06),
+            offset: const Offset(0, 2),
+            blurRadius: 8,
+            spreadRadius: 0,
+          ),
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.04),
+            offset: const Offset(0, 8),
+            blurRadius: 16,
+            spreadRadius: -4,
+          ),
+        ],
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          ...widget.destinations.map((e) => GestureDetector(
-            onTap: () {
-              final index = widget.destinations.indexOf(e);
-              setState(() {
-                selectedIndex = index;
-              }); 
-              // Check if the tapped item has its own onTap handler
-          if (widget.destinations.length > index && e.onTap != null) {
-            e.onTap!(index);
-          }
-          // Otherwise use the default handler
-          else if (widget.onTap != null) {
-            widget.onTap!(index);
-          } else {
-            widget.onPrimaryMenuSelected((widget.destinations[index].key as ValueKey).value);
-            if (e.mainSecondaryKey != null) {
-              widget.onSecondaryMenuSelected(e.mainSecondaryKey!);
-            }
-          }
-            },
-            child: Container(
-              padding: EdgeInsets.all($constants.insets.xxs),
-              // always make the center button have a wrapper
-              decoration: 2 == widget.destinations.indexOf(e) ? BoxDecoration(
-                color: Colors.grey.shade100,
-                borderRadius: BorderRadius.circular($constants.corners.full),
-                boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.06),
-                offset: const Offset(0, 0),
-                blurRadius: 2,
-                spreadRadius: 0,
-              ),
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.04),
-                offset: const Offset(0, 0),
-                blurRadius: 4,
-                spreadRadius: -4,
-              ),
-            ]
-              ) : null,
+          ...widget.destinations.map(
+            (e) => GestureDetector(
+              onTap: () {
+                final index = widget.destinations.indexOf(e);
+                setState(() {
+                  selectedIndex = index;
+                });
+                // Check if the tapped item has its own onTap handler
+                if (widget.destinations.length > index && e.onTap != null) {
+                  e.onTap!(index);
+                }
+                // Otherwise use the default handler
+                else if (widget.onTap != null) {
+                  widget.onTap!(index);
+                } else {
+                  widget.onPrimaryMenuSelected(
+                    (widget.destinations[index].key as ValueKey).value,
+                  );
+                  if (e.mainSecondaryKey != null) {
+                    widget.onSecondaryMenuSelected(e.mainSecondaryKey!);
+                  }
+                }
+              },
               child: Container(
-                padding: EdgeInsets.all($constants.insets.xs),
-                decoration: 2 == widget.destinations.indexOf(e) ? BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular($constants.corners.full),
-                boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.06),
-                offset: const Offset(0, 2),
-                blurRadius: 2,
-                spreadRadius: 0,
-              ),
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.04),
-                offset: const Offset(0, 8),
-                blurRadius: 4,
-                spreadRadius: -4,
-              ),
-            ]
-              ) : null,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(e.icon, color: selectedIndex == widget.destinations.indexOf(e) ? Colors.grey.shade800 : Colors.grey.shade600, size: 25,),
-                    if (widget.destinations.indexOf(e) != 2) Padding(
-                      padding: EdgeInsets.only(top: 2),
-                      child: Text(e.label, style: getTextTheme(context).bodyMedium?.copyWith(fontSize: 10, fontWeight: FontWeight.w700, color: selectedIndex == widget.destinations.indexOf(e) ? Colors.grey.shade800 : Colors.grey.shade600),),
-                    ),
-                  ],
+                padding: EdgeInsets.all($constants.insets.xxs),
+                // always make the center button have a wrapper
+                decoration:
+                    2 == widget.destinations.indexOf(e)
+                        ? BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(
+                            $constants.corners.full,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.06),
+                              offset: const Offset(0, 0),
+                              blurRadius: 2,
+                              spreadRadius: 0,
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.04),
+                              offset: const Offset(0, 0),
+                              blurRadius: 4,
+                              spreadRadius: -4,
+                            ),
+                          ],
+                        )
+                        : null,
+                child: Container(
+                  padding: EdgeInsets.all($constants.insets.xs),
+                  decoration:
+                      2 == widget.destinations.indexOf(e)
+                          ? BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(
+                              $constants.corners.full,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.06),
+                                offset: const Offset(0, 2),
+                                blurRadius: 2,
+                                spreadRadius: 0,
+                              ),
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.04),
+                                offset: const Offset(0, 8),
+                                blurRadius: 4,
+                                spreadRadius: -4,
+                              ),
+                            ],
+                          )
+                          : null,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        getIcon(e.icon, e.cupertinoIcon),
+                        color:
+                            selectedIndex == widget.destinations.indexOf(e)
+                                ? Colors.grey.shade800
+                                : Colors.grey.shade600,
+                        size: 25,
+                      ),
+                      if (widget.destinations.indexOf(e) != 2)
+                        Padding(
+                          padding: EdgeInsets.only(top: 2),
+                          child: Text(
+                            e.label,
+                            style: getTextTheme(context).bodyMedium?.copyWith(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w700,
+                              color:
+                                  selectedIndex ==
+                                          widget.destinations.indexOf(e)
+                                      ? Colors.grey.shade800
+                                      : Colors.grey.shade600,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          )),
+          ),
         ],
       ),
     );
+  }
+
+  IconData getIcon(IconData iconData, IconData cupertinoIcon) {
+    if (!kIsWasm && (Platform.isIOS || Platform.isMacOS)) {
+      return cupertinoIcon;
+    } else {
+      return iconData;
+    }
   }
 }
