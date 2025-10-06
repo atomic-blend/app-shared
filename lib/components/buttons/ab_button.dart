b7999f48fab4b574d746aaa6fdb2c992a3a48b26
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-06 15:34:44 +0200

    feat: add padding and optional label to ab button
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/components/buttons/ab_button.dart
+++ lib/components/buttons/ab_button.dart
@@ -4,19 +4,21 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 class ABButton extends StatefulWidget {
-  final String label;
+  final String? label;
   final IconData icon;
   final double? iconSize;
+  final EdgeInsets? padding;
   final VoidCallback onTap;
   final bool? enabled;
 
   const ABButton({
     super.key,
-    required this.label,
+    this.label,
     required this.icon,
     required this.onTap,
     this.iconSize = 15,
     this.enabled = true,
+    this.padding,
   });
 
   @override
@@ -43,19 +45,25 @@ class _ABButtonState extends State<ABButton> {
         onTap: widget.onTap,
         child: Container(
           decoration: BoxDecoration(
-            color: isHovering
-                ? getTheme(context).surfaceContainer.darken(5)
-                : getTheme(context).surfaceContainer,
+            color:
+                isHovering
+                    ? getTheme(context).surfaceContainer.darken(5)
+                    : getTheme(context).surfaceContainer,
             borderRadius: BorderRadius.circular($constants.corners.sm),
           ),
-          padding: EdgeInsets.symmetric(
-              horizontal: $constants.insets.sm,
-              vertical: $constants.insets.xxs),
+          padding:
+              widget.padding ??
+              EdgeInsets.symmetric(
+                horizontal: $constants.insets.sm,
+                vertical: $constants.insets.xxs,
+              ),
           child: Row(
             children: [
               Icon(widget.icon, size: widget.iconSize),
-              SizedBox(width: $constants.insets.xs),
-              Text(widget.label),
+              if (widget.label != null) ...[
+                SizedBox(width: $constants.insets.xs),
+                Text(widget.label!),
+              ],
             ],
           ),
         ),
