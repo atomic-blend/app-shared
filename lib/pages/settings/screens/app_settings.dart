9993b3d2e1342286aecaf58bd08b8fe4288e2af4
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-07 15:36:38 +0200

    feat: add settings screen with additional items
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

---
+++ lib/pages/settings/screens/app_settings.dart
@@ -0,0 +1,78 @@
+import 'package:ab_shared/components/buttons/icon_text_button.dart';
+import 'package:ab_shared/components/modals/edit_self_hosted_url_modal.dart';
+import 'package:ab_shared/i18n/strings.g.dart';
+import 'package:ab_shared/utils/api_client.dart';
+import 'package:ab_shared/utils/constants.dart';
+import 'package:ab_shared/utils/shortcuts.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettings extends StatefulWidget {
+  final SharedPreferences? prefs;
+  final ApiClient? globalApiClient;
+  const AppSettings({super.key, this.prefs, this.globalApiClient});
+
+  @override
+  State<AppSettings> createState() => _AppSettingsState();
+}
+
+class _AppSettingsState extends State<AppSettings> {
+  String? selfHostedUrl;
+
+  @override
+  void initState() {
+    getSelfHostedUrl();
+    super.initState();
+  }
+
+  getSelfHostedUrl() {
+    setState(() {
+      selfHostedUrl = widget.prefs?.getString("self_hosted_rest_api_url");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          spacing: $constants.insets.xs,
+          children: [
+            const Icon(LineAwesome.mobile_solid),
+            Text(context.t.settings.app_settings.title,
+                style: getTextTheme(context).bodyLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    )),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: $constants.insets.sm, vertical: $constants.insets.sm),
+        child: Column(
+          children: [
+            IconTextButton(
+              icon: LineAwesome.link_solid,
+              text: context.t.settings.app_settings.selfHostedUrl.title,
+              data: selfHostedUrl == "" || selfHostedUrl == null
+                  ? context.t.settings.app_settings.selfHostedUrl.not_set
+                  : selfHostedUrl,
+              onTap: () async {
+                await showDialog(
+                    context: context,
+                    builder: (context) => EditSelfHostedUrlModal(
+                          globalApiClient: widget.globalApiClient!,
+                          selfHostedUrl:
+                              widget.prefs?.getString("self_hosted_rest_api_url"),
+                        ));
+                getSelfHostedUrl();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/settings/settings.dart
@@ -0,0 +1,117 @@
+import 'package:ab_shared/blocs/auth/auth.bloc.dart';
+import 'package:ab_shared/components/app/ab_navbar.dart';
+import 'package:ab_shared/components/buttons/icon_text_button.dart';
+import 'package:ab_shared/i18n/strings.g.dart';
+import 'package:ab_shared/pages/settings/screens/app_settings.dart';
+import 'package:ab_shared/utils/api_client.dart';
+import 'package:ab_shared/utils/constants.dart';
+import 'package:ab_shared/utils/shortcuts.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Settings extends StatefulWidget {
+  final SharedPreferences? prefs;
+  final ApiClient? globalApiClient;
+  final List<Widget>? additionalSettings;
+  const Settings({
+    super.key,
+    this.prefs,
+    this.globalApiClient,
+    this.additionalSettings,
+  });
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class SharedPreferences {}
+
+class _SettingsState extends State<Settings> {
+  Widget? selectedItem;
+
+  @override
+  void initState() {
+    super.initState();
+    if (isDesktop(context)) {
+      selectedItem = AppSettings();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: isDesktop(context) ? 300 : getSize(context).width,
+          child: Scaffold(
+            body: BlocBuilder<AuthBloc, AuthState>(
+              builder: (contex, authState) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: $constants.insets.sm,
+                    vertical: $constants.insets.xs,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: $constants.insets.xs,
+                      horizontal: $constants.insets.xxs,
+                    ),
+                    child: Column(
+                      spacing: $constants.insets.md,
+                      children: [
+                        IconTextButton(
+                          text: context.t.settings.app_settings.title,
+                          icon: CupertinoIcons.device_phone_portrait,
+                          iconSize: 20,
+                          onTap: () {
+                            if (isDesktop(context)) {
+                              setState(() {
+                                selectedItem = AppSettings();
+                              });
+                            } else {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return AppSettings();
+                                  },
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                        if (widget.additionalSettings != null) ...[
+                          ...widget.additionalSettings!,
+                        ],
+                        if (authState is LoggedIn)
+                          IconTextButton(
+                            text: context.t.settings.logout,
+                            icon: CupertinoIcons.multiply_circle_fill,
+                            iconSize: 20,
+                            iconColor: Colors.red,
+                            textColor: Colors.red,
+                            onTap: () {
+                              context.read<AuthBloc>().add(const Logout());
+                              Navigator.pop(context);
+                            },
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        if (isDesktop(context)) ...[
+          VerticalDivider(
+            color: getTheme(context).surfaceContainer,
+            thickness: 1,
+          ),
+          Expanded(child: selectedItem ?? Container()),
+        ],
+      ],
+    );
+  }
+}
