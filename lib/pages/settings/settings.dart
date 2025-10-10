a6999997be2d25aabafeed11f27b8dcdc883cc3f
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-10 15:40:15 +0200

    fix: migrate settings to getIt
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -2,23 +2,18 @@ import 'package:ab_shared/blocs/auth/auth.bloc.dart';
 import 'package:ab_shared/components/buttons/icon_text_button.dart';
 import 'package:ab_shared/i18n/strings.g.dart';
 import 'package:ab_shared/pages/settings/screens/app_settings.dart';
-import 'package:ab_shared/utils/api_client.dart';
 import 'package:ab_shared/utils/constants.dart';
 import 'package:ab_shared/utils/shortcuts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'settings.g.dart';
 
 class SettingsParams {
   final List<Widget>? additionalSettings;
-  const SettingsParams({
-    this.additionalSettings,
-  });
+  const SettingsParams({this.additionalSettings});
 }
 
 @TypedGoRoute<SettingsRoute>(path: "/settings", name: "settings")
@@ -28,24 +23,13 @@ class SettingsRoute extends GoRouteData with _$SettingsRoute {
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return Settings(
-      additionalSettings: $extra?.additionalSettings,
-    );
+    return Settings(additionalSettings: $extra?.additionalSettings);
   }
 }
 
 class Settings extends StatefulWidget {
-  final getIt = GetIt.instance;
-  late final SharedPreferences? prefs;
-  late final ApiClient? globalApiClient;
-  final  List<Widget>? additionalSettings;
-  Settings({
-    super.key,
-    this.additionalSettings,
-  }) {
-    prefs = getIt<SharedPreferences>();
-    globalApiClient = getIt<ApiClient>();
-  }
+  final List<Widget>? additionalSettings;
+  const Settings({super.key, this.additionalSettings});
 
   @override
   State<Settings> createState() => _SettingsState();
@@ -61,16 +45,8 @@ class _SettingsState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget.prefs == null || widget.globalApiClient == null) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        context.go('/');
-      });
-    }
     if (isDesktop(context) && selectedItem == null) {
-      selectedItem = AppSettings(
-        prefs: widget.prefs,
-        globalApiClient: widget.globalApiClient,
-      );
+      selectedItem = AppSettings();
     }
     return Row(
       children: [
@@ -99,20 +75,14 @@ class _SettingsState extends State<Settings> {
                           onTap: () {
                             if (isDesktop(context)) {
                               setState(() {
-                                selectedItem = AppSettings(
-                                  prefs: widget.prefs,
-                                  globalApiClient: widget.globalApiClient,
-                                );
+                                selectedItem = AppSettings();
                               });
                             } else {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) {
-                                    return AppSettings(
-                                      prefs: widget.prefs,
-                                      globalApiClient: widget.globalApiClient,
-                                    );
+                                    return AppSettings();
                                   },
                                 ),
                               );
